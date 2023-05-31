9b9653dc988a8aa34ee516f6ab49122e7f378095
samuelselasi <samelsekasi@gmail.com>
2023-05-31 12:39:02 +0000

    Add function that checks if a binary tree is a valid AVL Tree

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -57,6 +57,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	l_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (l_height > r_height ? l_height : r_height);
 }
