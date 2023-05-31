b1859d3c08899fc23958011bb4dbde4bb2ec771f
samuelselasi <samelsekasi@gmail.com>
2023-05-31 23:13:04 +0000

    Add function that inserts a value in Max Binary Heap

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -12,7 +12,7 @@ size_t binary_tree_size(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *tree, *new_node, *swap;
-	int size, leaves, sub, bit, level, tmp;
+	int tmp, level, bit, sub, leaves, size;
 
 	if (!root)
 		return (NULL);
